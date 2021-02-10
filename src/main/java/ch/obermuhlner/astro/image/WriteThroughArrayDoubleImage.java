bb1b522bbd7d24c86cd3a8d2f35b8271d411b590
Eric Obermühlner <eric.obermuehlner@assetmax.ch>
2021-02-10 08:08:15 +0100

    add WriteThroughArrayDoubleImage

---
+++ src/main/java/ch/obermuhlner/astro/image/WriteThroughArrayDoubleImage.java
@@ -0,0 +1,34 @@
+package ch.obermuhlner.astro.image;
+
+public class WriteThroughArrayDoubleImage implements DoubleImage {
+
+  private final DoubleImage image;
+  private final ArrayDoubleImage arrayImage;
+
+  public WriteThroughArrayDoubleImage(DoubleImage image, ColorModel colorModel) {
+    this.image = image;
+
+    this.arrayImage = new ArrayDoubleImage(image.getWidth(), image.getHeight(), colorModel);
+  }
+
+  @Override
+  public int getWidth() {
+    return arrayImage.getWidth();
+  }
+
+  @Override
+  public int getHeight() {
+    return arrayImage.getHeight();
+  }
+
+  @Override
+  public double[] getPixel(int x, int y, ColorModel model, double[] samples) {
+    return arrayImage.getPixel(x, y, model, samples);
+  }
+
+  @Override
+  public void setPixel(int x, int y, ColorModel model, double[] samples) {
+    arrayImage.setPixel(x, y, model, samples);
+    image.setPixel(x, y, model, samples);
+  }
+}

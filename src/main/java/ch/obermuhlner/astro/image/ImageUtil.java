5ea0dd29c6b7e041729fd5cc80061079421799c3
Eric Obermühlner <eric.obermuehlner@assetmax.ch>
2021-02-18 07:42:02 +0100

    add medianPixelPerSample()

--- src/main/java/ch/obermuhlner/astro/image/ImageUtil.java
+++ src/main/java/ch/obermuhlner/astro/image/ImageUtil.java
@@ -1,6 +1,12 @@
 package ch.obermuhlner.astro.image;
 
+import java.util.Arrays;
+
 public class ImageUtil {
+
+  public static void copyPixels(DoubleImage source, DoubleImage target, ColorModel model) {
+    copyPixels(source, 0, 0, target, 0, 0, source.getWidth(), source.getHeight(), model);
+  }
 
   public static void copyPixels(DoubleImage source, int sourceX, int sourceY, DoubleImage target, int targetX, int targetY, int width, int height, ColorModel model) {
     double[] samples = new double[3];
@@ -44,6 +50,49 @@ public class ImageUtil {
         }
       }
     }
-    return new double[] { sample0/n, sample1/n, sample2/n };
+
+    color[0] = sample0 / n;
+    color[1] = sample1 / n;
+    color[2] = sample2 / n;
+    return color;
   }
+
+  public static double[] medianPixelPerSample(DoubleImage image, ColorModel colorModel, double[] color) {
+    if (color == null) {
+      color = new double[3];
+    }
+
+    int n = image.getWidth() * image.getHeight();
+    double[] values0 = new double[n];
+    double[] values1 = new double[n];
+    double[] values2 = new double[n];
+
+    int index = 0;
+    for (int y = 0; y < image.getHeight(); y++) {
+      for (int x = 0; x < image.getWidth(); x++) {
+        image.getPixel(x, y, colorModel, color);
+        values0[index] = color[0];
+        values1[index] = color[1];
+        values2[index] = color[2];
+        index++;
+      }
+    }
+
+    Arrays.sort(values0);
+    Arrays.sort(values1);
+    Arrays.sort(values2);
+
+    if (n % 2 == 0) {
+      color[0] = (values0[n/2] + values0[n/2+1]) / 2;
+      color[1] = (values1[n/2] + values1[n/2+1]) / 2;
+      color[2] = (values2[n/2] + values2[n/2+1]) / 2;
+    } else {
+      color[0] = values0[n/2];
+      color[1] = values1[n/2];
+      color[2] = values2[n/2];
+    }
+
+    return color;
+  }
+
 }
